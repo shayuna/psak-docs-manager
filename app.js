c5ebf97f8badf42bb31d953031a98adeb3e1c6ce
shayuna <shaiz2206@gmail.com>
2017-01-09 23:06:45 +0200

    new try

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+  res.status(200).send('hey, what\'s up!');
 });
 
 // Start the server
